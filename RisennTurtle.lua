745da28ef8a4b59be575364e38dd6d2e088ba69c
Sven Kirmess <sven.kirmess@kzone.ch>
2025-06-15 22:08:56 +0200

    add /travelherb command

--- RisennTurtle.lua
+++ RisennTurtle.lua
@@ -17,6 +17,28 @@ local function Risenn_CancelShadowForm()
 	end
 end
 
+local function Risenn_TravelWithHerbs()
+	if not GetTrackingTexture()
+	then
+		CastSpellByName("Find Herbs")
+	end
+
+	local _,_,a = GetShapeshiftFormInfo(4)
+	if a then
+		CastShapeshiftForm(4)
+	else
+		local _, _, a = GetShapeshiftFormInfo(2)
+		if a then
+			CastShapeshiftForm(2)
+		else
+			UIErrorsFrame:UnregisterEvent"UI_ERROR_MESSAGE"
+			CastShapeshiftForm(2)
+			CastShapeshiftForm(4)
+			UIErrorsFrame:RegisterEvent"UI_ERROR_MESSAGE"
+		end
+	end
+end
+
 initFrame:SetScript("OnEvent", function()
 
 	if ( event == "PLAYER_ENTERING_WORLD" ) then
@@ -24,6 +46,9 @@ initFrame:SetScript("OnEvent", function()
 
 		SLASH_RISENN1 = "/cancelshadowform"
 		SlashCmdList["RISENN"] = function() Risenn_CancelShadowForm(msg) end
+
+		SLASH_TRAVELHERB1 = "/travelherb"
+		SlashCmdList["TRAVELHERB"] = function() Risenn_TravelWithHerbs(msg) end
 	end
 
 end)
